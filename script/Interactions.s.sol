3f7363af722ca5e3faa8cc00a5e06158a74b215d
oladele12 <oladeleoripinye@gmail.com>
2025-08-22 18:49:28 +0100

    Made changes in interactionsTest.t.sol file

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -7,13 +7,11 @@ import {DevOpsTools} from "../lib/foundry-devops/src/DevOpsTools.sol";
 import {FundMe} from "../src/FundMe.sol";
 
 contract FundFundMe is Script {
-    uint256 constant SEND_value = 0.1 ether; // 0.1 ETH in wei
+    uint256 constant SEND_VALUE = 0.1 ether; // 0.1 ETH in wei
 
     function fundFundMe(address mostRecentDeployed) public {
-        vm.startBroadcast();
-        FundMe(payable(mostRecentDeployed)).fund{value: SEND_value}();
-        vm.stopBroadcast();
-        console.log("Funded FundMe contract %s", SEND_value);
+        FundMe(payable(mostRecentDeployed)).fund{value: SEND_VALUE}();
+        console.log("Funded FundMe contract %s", SEND_VALUE);
     }
 
     function run() external {
@@ -21,8 +19,28 @@ contract FundFundMe is Script {
             "FundMe",
             block.chainid
         );
+        vm.startBroadcast();
         fundFundMe(mostRecentlyDeployed);
+        vm.stopBroadcast();
     }
 }
 
-contract WithdrwFundMe is Script {}
+contract WithdrawFundMe is Script {
+    uint256 constant SEND_VALUE = 0.1 ether; // 0.1 ETH in wei
+
+    function withdrawFundMe(address mostRecentDeployed) public {
+        vm.startBroadcast();
+        FundMe(payable(mostRecentDeployed)).withdraw();
+        vm.stopBroadcast();
+    }
+
+    function run() external {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
+            "FundMe",
+            block.chainid
+        );
+        vm.startBroadcast();
+        WithdrawFundMe(mostRecentlyDeployed);
+        vm.stopBroadcast();
+    }
+}
--- test/integration test/InteractionsTest.t.sol
+++ test/integration test/InteractionsTest.t.sol
@@ -7,9 +7,9 @@ import {FundMe} from "../../src/FundMe.sol";
 import {StdAssertions} from "forge-std/StdAssertions.sol";
 import {PriceConverter} from "../../src/PriceConverter.sol";
 import {DeployFundMe} from "../../script/DeployFundMe.s.sol";
-import {FundFundMe} from "../../script/Interactions.s.sol";
+import {FundFundMe, WithdrawFundMe} from "../../script/Interactions.s.sol";
 
-contract InteractionTest is Test {
+contract InteractionsTest is Test {
     FundMe fundMe;
 
     address USER = makeAddr("user");
@@ -20,11 +20,29 @@ contract InteractionTest is Test {
         // fundMe = new FundMe();
         DeployFundMe deploy = new DeployFundMe();
         fundMe = deploy.run();
+        vm.deal(USER, STARTING_BALANCE);
     }
 
-    function testUserCanFund() external {
-        FundFundMe fundFundMe = new FundFundMe();
-        fundFundMe.fundFundMe(address(fundMe));
+    function testUserCanFundAndOwnerWithdraw() public {
+        uint256 preUserBalance = address(USER).balance;
+        uint256 preOwnerBalance = address(fundMe.getOwner()).balance;
+        uint256 originalFundMeBalance = address(fundMe).balance;
+
+        // Using vm.prank to simulate funding from the USER address
+        vm.prank(USER);
+        fundMe.fund{value: SEND_VALUE}();
+
+        WithdrawFundMe withdrawFundMe = new WithdrawFundMe();
+        withdrawFundMe.withdrawFundMe(address(fundMe));
+
+        uint256 afterUserBalance = address(USER).balance;
+        uint256 afterOwnerBalance = address(fundMe.getOwner()).balance;
+
+        assert(address(fundMe).balance == 0);
+        assertEq(afterUserBalance + SEND_VALUE, preUserBalance);
+        assertEq(
+            preOwnerBalance + SEND_VALUE + originalFundMeBalance,
+            afterOwnerBalance
+        );
     }
 }
-
